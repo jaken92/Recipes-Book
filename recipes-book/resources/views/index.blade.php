5a35d9b7d33f881eab8161097735a6ad8fc3bc20
jaken92 <jaaakeeen@gmail.com>
2023-02-23 12:03:57 +0100

    index.blade.php created

---
+++ recipes-book/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+
+<form action="/login" method="post">
+    <div>
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" />
+    </div>
+    <button type="submit">Login</button>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+</form>
