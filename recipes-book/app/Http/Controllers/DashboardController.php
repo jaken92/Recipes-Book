eae5ef7fb3b555b0445cd6d8d0fb420f60384cc9
jaken92 <jaaakeeen@gmail.com>
2023-02-28 14:18:59 +0100

    controllers

--- recipes-book/app/Http/Controllers/DashboardController.php
+++ recipes-book/app/Http/Controllers/DashboardController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     /**
      * Handle the incoming request.
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        //
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user = $request->user();
+            // print_r($user);            
+            return view('dashboard', ['user' => $user]);
+        }
     }
 }
--- recipes-book/routes/web.php
+++ recipes-book/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/', 'index')->name('login')->middleware("guest");
 
 Route::get('dashboard', DashboardController::class)->middleware('auth');
