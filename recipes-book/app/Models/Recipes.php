f12ddc5c8facc4e8bb56eb112de7e0b8f5c51e55
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-02-28 12:59:36 +0100

    created models

---
+++ recipes-book/app/Models/Recipes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipes extends Model
+{
+    use HasFactory;
+}
