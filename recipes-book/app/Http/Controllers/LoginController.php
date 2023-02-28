3bc6f08562fc299caffc5df057eb5686a0d670c6
jaken92 <jaaakeeen@gmail.com>
2023-02-28 14:08:41 +0100

    login controller, added route for dashboard

--- recipes-book/app/Http/Controllers/LoginController.php
+++ recipes-book/app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -11,6 +12,16 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $credentials = $request->only("email", "password");
+
+        if (Auth::attempt($credentials)) {
+            // redirect to dashboard
+            $request->session()->regenerate();
+
+            return redirect()->intended("dashboard");
+        }
+        // redirect to index
+        return back()->withErrors("Woops! Please try login again");
+        // Woops! Please try login again
     }
 }
