6704bbfedbc6db086ee7972258bb00b4676a0ce5
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-03-02 15:46:21 +0100

    can now se if ingredient already exist

--- recipes-book/app/Http/Controllers/AddIngredientToDbController.php
+++ recipes-book/app/Http/Controllers/AddIngredientToDbController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ingredients;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+use function PHPUnit\Framework\isNull;
 
 class AddIngredientToDbController extends Controller
 {
@@ -17,18 +21,33 @@ class AddIngredientToDbController extends Controller
         ]);
 
         $newIngredient = $request->only(['new-ingredient']);
-        /* if (Auth::check()) {
+
+        if (Auth::check()) {
             $user = Auth::user();
             $user = $request->user();
-            // print_r($user); 
 
+            $ingredient = DB::table('ingredients')->where('name', $newIngredient['new-ingredient'])->first();
+            // print_r($user);
+            // print_r($ingredients);
+            // var_dump($ingredient);
+            if (is_null($ingredient)) {
+                // echo "not exist";
+                return back()->withErrors("Ingredient already exists, check the dropdown menu.");
+            } else if ($ingredient->name == $newIngredient['new-ingredient']) {
+                echo "exist";
+
+
+                return back();
+            } else {
+                // echo "not exist";
+                return back()->withErrors("Ingredient already exists, check the dropdown menu.");
+            }
             // return redirect('/recipe');
-            return back();
-        } */
+            // return back();
+        }
 
         // return back();
 
-        var_dump($newIngredient);
-        // return back()->withErrors("Ingredient already exists, check the dropdown menu.");
+        // var_dump($newIngredient);        
     }
 }
