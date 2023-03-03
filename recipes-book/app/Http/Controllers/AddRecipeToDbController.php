e3faa437addf2e6e478b6247779e5acfa02e424e
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-03-03 13:42:52 +0100

    changed things in controllers

--- recipes-book/app/Http/Controllers/AddRecipeToDbController.php
+++ recipes-book/app/Http/Controllers/AddRecipeToDbController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ingredients;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AddRecipeToDbController extends Controller
 {
@@ -12,5 +15,39 @@ class AddRecipeToDbController extends Controller
     public function __invoke(Request $request)
     {
         //
+        $this->validate($request, [
+            'new-ingredient' => 'required|string|min:2'
+        ]);
+
+        $newIngredient = $request->only(['new-ingredient']);
+
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user = $request->user();
+
+            $ingredient = DB::table('ingredients')->where('name', $newIngredient['new-ingredient'])->first();
+            // print_r($user);
+            // print_r($ingredients);
+            // var_dump($ingredient);
+            if (is_null($ingredient)) {
+                DB::table('ingredients')
+                    ->insert([
+                        'name' => $newIngredient['new-ingredient'],
+                        "created_at" =>  date('Y-m-d H:i:s', strtotime("+1 hours")),
+                        "updated_at" => date('Y-m-d H:i:s', strtotime("+1 hours")),
+
+                    ]);
+            } else if ($ingredient->name == $newIngredient['new-ingredient']) {
+
+                return back()->withErrors("Could not add your recipe");
+            }
+
+            // return redirect('/recipe');
+            return back();
+        }
+
+        // return back();
+
+        // var_dump($newIngredient);        
     }
 }
