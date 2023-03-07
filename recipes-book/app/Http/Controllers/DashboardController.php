2de6f1475725bfd14d7a333789a34698eb410df2
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-03-07 15:49:51 +0100

    shows all recipes on dashboard

--- recipes-book/app/Http/Controllers/DashboardController.php
+++ recipes-book/app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -18,9 +19,42 @@ class DashboardController extends Controller
             $user = Auth::user();
             $user = $request->user();
 
-            return view('dashboard', ['user' => $user]);
+            $categories = DB::table('categories')->get();
+            $ingredients = DB::table('ingredients')->get();
+            $recipes = DB::table('recipes')->get();
+            $recipeIngredients = DB::table('recipe_ingredients')->get();
+            $users = DB::table('users')->select('id', 'name')->get();
+
+            $recipes = DB::table('recipe_ingredients')->select('*')
+                ->join('recipes', 'recipes.id', '=', 'recipe_ingredients.recipe_id')
+                ->join('ingredients', 'ingredients.id', '=', 'recipe_ingredients.ingredient_id')
+                ->get();
+
+            // ->where('recipe_id', '=', 34)
+            // return view('dashboard')->with('user', $user)->with('categories', $categories)->with('ingredients', $ingredients)->with('recipes', $recipes)->with('recipe_ingredients', $recipeIngredients)->with('users', $users);
+
+            foreach ($recipes as $key => $recipe) {
+
+                /* echo $recipe->id;
+                echo $recipe->title;
+                echo $recipe->instructions;
+                echo $recipe->category_id;
+                echo $recipe->ingredient_id;
+                echo $recipe->name;
+                echo $recipe->amount;
+                echo $recipe->unit; */
+
+                /* echo "recipe id: " . $recipe->recipe_id . " recipe title: " . $recipe->title . " recipe instructions: " . $recipe->instructions . " recipe category: " . $recipe->category_id . " ingredient id: " . $recipe->ingredient_id . " ingredient name: " . $recipe->name . " amount: " . $recipe->amount . $recipe->unit . " unit: ";
+                echo "<br>"; */
+                // echo $recipe->user_id;
+            }
+
+            // var_dump($recipes);
+            return view('dashboard')->with('recipes', $recipes);
+
+            // return view('dashboard', ['user' => $user]);
         }
 
-        return redirect('/');
+        // return redirect('/');
     }
 }
--- recipes-book/resources/views/dashboard.blade.php
+++ recipes-book/resources/views/dashboard.blade.php
@@ -6,6 +6,15 @@
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 </form>
 
+@foreach ($recipes as $recipe)
+<a href="" data-recipeId="{{$recipe->recipe_id}}" style="display: block;">{{$recipe->title}}</a>
+@endforeach
+
+<!-- @foreach ($recipes as $recipe)
+<p><?php echo "recipe id: " . $recipe->recipe_id . " recipe title: " . $recipe->title . " recipe instructions: " . $recipe->instructions . " recipe category: " . $recipe->category_id . " ingredient id: " . $recipe->ingredient_id . " ingredient name: " . $recipe->name . " amount: " . $recipe->amount . $recipe->unit . " unit: ";
+    echo "<br>"; ?></p>
+@endforeach -->
+
 <form action="addRecipe" method="get">
     <button>Go to add Recipe page</button>
     <!-- <input type="hidden" name="_token" value="{{ csrf_token() }}" /> -->
