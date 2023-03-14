af838a9614223117b2e8e579c821fc7a09f88494
jaken92 <jaaakeeen@gmail.com>
2023-03-14 15:26:12 +0100

    css and js

--- recipes-book/public/home.js
+++ recipes-book/public/home.js
@@ -1,7 +1,9 @@
 const checkboxes = document.querySelector(".checkboxes");
 const btn = document.querySelector(".show-checkboxes");
+const resetBtn = document.querySelector(".reset-btn");
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("hidden");
     checkboxes.classList.toggle("hidden");
+    resetBtn.classList.toggle("hidden");
 });
