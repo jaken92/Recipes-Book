bade5685abe0cb869858f58b7552e906b8cea4c8
jaken92 <jaaakeeen@gmail.com>
2023-03-15 15:05:08 +0100

    .

--- recipes-book/resources/views/registerUser.blade.php
+++ recipes-book/resources/views/registerUser.blade.php
@@ -6,19 +6,21 @@
         <img class="hero" src="/images/hero.jpg" alt="">
         <h1 class="hero-text">Recipes Book</h1>
     </div>
-    <form method="post" action="saveUser">
-        <label for="email">Enter email</label>
-        <input name="email" type="text">
-
-        <label for="name">Enter username</label>
-        <input name="name" type="text">
-
-        <label for="">Enter password</label>
-        <input name="password" type="text">
-
-        <button type="submit">sumbit</button>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-    </form>
+    <div class="register-user-wrapper">
+        <form method="post" action="saveUser">
+            <label for="email">Enter email</label>
+            <input name="email" type="text">
+    
+            <label for="name">Enter username</label>
+            <input name="name" type="text">
+    
+            <label for="">Enter password</label>
+            <input name="password" type="text">
+    
+            <button type="submit">sumbit</button>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+        </form>
+    </div>
     @if(\Session::has('success'))
     <p>{!! \Session::get('success') !!}</p>
     @endif
